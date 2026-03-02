8a4abd5f040a44ef0639c79946b560d9369d4638
theo-p38 <theo.patrac@yahoo.com>
2026-03-03 00:08:15 +0100

    Add Certbot RFC2136 command for staging wildcard certificate

---
+++ test/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#sudo apt-get update
+#sudo apt-get install -y bind9 dnsutils
+
+#certbot certonly \
+#--dns-rfc2136 \
+#--dns-rfc2136-credentials <credentials_path> \
+#--staging -d *.<targeted_domain>
